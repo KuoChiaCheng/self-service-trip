fd60f56691650048ee65a0aab92f92ec3eed5aa8
KuoChiaCheng <kuochiachen@gmail.com>
2016-11-22 19:39:45 +0800

    20161122 food

--- D3JS.js
+++ D3JS.js
@@ -3,27 +3,27 @@ function LineChart()
 	var width  = 460;
 	var height = 268;
 	dataPath = 'https://kuochiacheng.github.io/self-service-trip/Rdata/';
-	dataFile = 'outputV2.csv';
+	dataFile = 'output1.csv';
 	dataUrl = dataPath + dataFile;
 
 d3.csv(dataUrl, function(data) {
 	console.log(data);
 	data.forEach(function(d) {
+		console.log(d.size);
 		console.log(d.word);
-		console.log(d.freq);
 	});
-	var maxFreq = d3.max(data, function(d) { return d.freq; });
+	var maxSize = d3.max(data, function(d) { return d.size; });
 	var ln = data.length;
 	var ctrl  = d3.select("#kuo").append("svg").attr("width", width).attr("height", height);
 	//定義freq畫線的規則
-	var linesFreq = d3.line().
+	var linesSize = d3.line().
 	x(function(d,i){ return i * (width/ln); }).  //寬值平均分配
-	y(function(d){ return height - d.freq * (height/maxFreq); });
+	y(function(d){ return height - d.size * (height/maxSize); });
 
 	//開始freq畫線
 	ctrl.append("path").data([data]).
 	attr("class", "pathline").  //補上pathline屬性讓CSS控制
-	attr("d", linesFreq);
+	attr("d", linesSize);
 });
 
 }
